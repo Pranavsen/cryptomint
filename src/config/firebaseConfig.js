c8530c3b714b2db4a82ef80b11e505370668866e
Pranav Sen <pranav.sen2019@vitbhopal.ac.in>
2023-04-04 22:00:41 +0530

    add firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAHKJDO4azjEtwYNw95tvoGDk8hz57S8QY",
+    authDomain: "crypto-hunter-c7320.firebaseapp.com",
+    projectId: "crypto-hunter-c7320",
+    storageBucket: "crypto-hunter-c7320.appspot.com",
+    messagingSenderId: "508454251687",
+    appId: "1:508454251687:web:95607693f016a36e20ff82"
+  };
+
+  export default firebaseConfig;
